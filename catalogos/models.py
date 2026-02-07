9e99b67afb5a40080e1aeebed31edf187f92b45a
carlostene-svg <carlos.tene@uisek.edu.ec>
2026-02-07 11:20:25 -0500

    modelos Autor y Libro con campos

--- catalogos/models.py
+++ catalogos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+    nacionalidad = models.CharField(max_length=50, blank=True, null=True)
+    bibliografia = models.TextField(blank=True)
+    imagen = models.ImageField(upload_to='autores/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+    
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='libros')
+    fecha_publicacion = models.DateField()
+    genero = models.CharField(max_length=50, blank=True, null=True)
+    resumen = models.TextField(blank=True)
+    portada = models.ImageField(upload_to='libros/', blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
