ea0807c8311b79fbb2344a45c4b1979a1a3f55ca
carlostene-svg <carlos.tene@uisek.edu.ec>
2026-02-07 11:43:51 -0500

    registro de autores y libros en admin

--- catalogos/admin.py
+++ catalogos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Autor, Libro
 
 # Register your models here.
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    pass
+    
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    pass
